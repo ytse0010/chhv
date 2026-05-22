7b2cd4a5532d66f4811fdf8f588de8c318115917
ytse0010 <ytse0010@student.monash.edu>
2026-05-22 12:46:11 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,76 +1,76 @@
-var vg1 = "vega/tree_location_map.vg.json";
+var vg1 = "tree_location_map.vg.json";
 vegaEmbed("#tree_location_map", vg1).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg2 = "vega/tree_bubble_map.vg.json";
+var vg2 = "tree_bubble_map.vg.json";
 vegaEmbed("#tree_bubble_map", vg2).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg3 = "vega/tree_count_precinct.vg.json";
+var vg3 = "tree_count_precinct.vg.json";
 vegaEmbed("#tree_count_precinct", vg3).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg4 = "vega/avg_diameter_precinct.vg.json";
+var vg4 = "avg_diameter_precinct.vg.json";
 vegaEmbed("#avg_diameter_precinct", vg4).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg5 = "vega/tree_scatter.vg.json";
+var vg5 = "tree_scatter.vg.json";
 vegaEmbed("#tree_scatter", vg5).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg6 = "vega/top_species_chart.vg.json";
+var vg6 = "top_species_chart.vg.json";
 vegaEmbed("#top_species_chart", vg6).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg7 = "vega/species_stacked_bar.vg.json";
+var vg7 = "species_stacked_bar.vg.json";
 vegaEmbed("#species_stacked_bar", vg7).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg8 = "vega/species_heatmap.vg.json";
+var vg8 = "species_heatmap.vg.json";
 vegaEmbed("#species_heatmap", vg8).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg9 = "vega/diameter_histogram.vg.json";
+var vg9 = "diameter_histogram.vg.json";
 vegaEmbed("#diameter_histogram", vg9).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg10 = "vega/life_expectancy_chart.vg.json";
+var vg10 = "life_expectancy_chart.vg.json";
 vegaEmbed("#life_expectancy_chart", vg10).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg11 = "vega/planting_year_chart.vg.json";
+var vg11 = "planting_year_chart.vg.json";
 vegaEmbed("#planting_year_chart", vg11).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg12 = "vega/planting_precinct_chart.vg.json";
+var vg12 = "planting_precinct_chart.vg.json";
 vegaEmbed("#planting_precinct_chart", vg12).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 
-var vg13 = "vega/current_vs_future_chart.vg.json";
+var vg13 = "current_vs_future_chart.vg.json";
 vegaEmbed("#current_vs_future_chart", vg13).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
